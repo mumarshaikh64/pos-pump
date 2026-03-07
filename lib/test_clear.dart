5f5d5cfb94559d4b79517dc8aebbd26aa91f3f7c
mumarshaikh64 <mumarshaikh64@gmail.com>
2026-03-07 11:36:21 +0500

    delete test files

--- lib/test_clear.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final prefs = await SharedPreferences.getInstance();
-  await prefs.remove('drive_credentials');
-  print('cleared!!!');
-}
